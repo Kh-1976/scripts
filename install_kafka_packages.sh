49e888d605e035411d5eff3be8b93351732b2260
Kh-1976 <78970676+Kh-1976@users.noreply.github.com>
2026-03-10 22:35:10 +0300

    Create install_kafka_packages.sh
    Рабочая версия

---
+++ install_kafka_packages.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Получаем имя пода scheduler
+SCHEDULER_NAME=$(minikube kubectl -- get pods -n airflow -l component=scheduler -o jsonpath="{.items[0].metadata.name}")
+# Получаем имя пода worker
+WORKER_POD=$(minikube kubectl -- get pods -n airflow -l component=worker -o jsonpath="{.items[0].metadata.name}")
+# Получаем имя пода dag-processor
+DAG_POD=$(minikube kubectl -- get pods -n airflow -l component=dag-processor -o jsonpath="{.items[0].metadata.name}")
+# Получаем имя пода triggerer
+TRIGGER_POD=$(minikube kubectl -- get pods -n airflow -l component=triggerer -o jsonpath="{.items[0].metadata.name}")
+
+# Функция для установки пакетов в под
+install_packages() {
+    local pod_name=$1
+    local pod_type=$2
+    
+    echo "Installing packages in $pod_type pod: $pod_name"
+    # -it не указывать. В интерактивном режиме после добавления библиотек в scheduler скрипт будет прирываться.
+    # То есть в интерактивном режиме после первого удачного выполнения происходит остановка.
+    minikube kubectl -- exec $pod_name -n airflow -- /bin/bash -c "
+        echo 'Upgrading pip...' &&
+        pip install --upgrade pip &&
+        echo 'Installing required packages...' &&
+        pip install confluent-kafka kafka-python faker &&
+        echo 'Package installation completed in $pod_type pod'
+    "
+}
+
+# Устанавливаем пакеты во все поды
+install_packages "$SCHEDULER_NAME" "scheduler"
+install_packages "$WORKER_POD" "worker"
+install_packages "$DAG_POD" "dag-processor"
+install_packages "$TRIGGER_POD" "triggerer"
+
+echo "All packages installed successfully in all pods!"
+
+
+Последний верный без функции
