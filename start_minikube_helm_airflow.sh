ae912f45f027189854844733eee44137c6481d5d
Kh-1976 <78970676+Kh-1976@users.noreply.github.com>
2026-03-10 22:49:26 +0300

    Update start_minikube_helm_airflow.sh
    Изменил на scripts

--- start_minikube_helm_airflow.sh
+++ start_minikube_helm_airflow.sh
@@ -13,6 +13,6 @@ echo "Ожидание завершено: $(date)"
 
 echo "=== Запуск скрипта install_kafka_packages.sh ==="
 # Загружаем и запускаем скрипт установки Kafka пакетов
-curl -s https://raw.githubusercontent.com/Kh-1976/minikube_data/main/install_kafka_packages.sh | bash
+curl -s https://raw.githubusercontent.com/Kh-1976/scripts/main/install_kafka_packages.sh | bash
 
 echo "=== Все операции завершены ==="
