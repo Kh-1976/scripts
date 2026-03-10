14e1fa9c3b78d795df45ab605730f755dc18ac89
Kh-1976 <78970676+Kh-1976@users.noreply.github.com>
2026-03-10 22:50:25 +0300

    Update first_start_helm_airflow.sh
    изменил на scripts

--- first_start_helm_airflow.sh
+++ first_start_helm_airflow.sh
@@ -34,6 +34,6 @@ minikube kubectl apply -- \
 
 echo "=== Запуск скрипта install_kafka_packages.sh ==="
 # Загружаем и запускаем скрипт установки Kafka пакетов
-curl -s https://raw.githubusercontent.com/Kh-1976/minikube_data/main/install_kafka_packages.sh | bash
+curl -s https://raw.githubusercontent.com/Kh-1976/scripts/main/install_kafka_packages.sh | bash
 
 echo "=== Все операции завершены ==="
