f437ee2a4bac7f77886f3d59f754d27be9dc183d
Kh-1976 <78970676+Kh-1976@users.noreply.github.com>
2026-03-20 00:03:06 +0300

    Update start_minikube_helm_airflow.sh
    #echo "=== Ожидание 2 минут для полного запуска всех подов ==="
    #echo "Ожидание началось: $(date)"
    #sleep 120  # 2 минут = 120 секунд
    #echo "Ожидание завершено: $(date)
    
    вернул ожидание. Поды не успевают запустится и поэтому не устанавливается в поды confluent_kafka

--- start_minikube_helm_airflow.sh
+++ start_minikube_helm_airflow.sh
@@ -6,6 +6,11 @@ helm repo update
 echo "=== Запуск Minikube ==="
 minikube start
 
+#echo "=== Ожидание 2 минут для полного запуска всех подов ==="
+#echo "Ожидание началось: $(date)"
+#sleep 120  # 2 минут = 120 секунд
+#echo "Ожидание завершено: $(date)"
+
 echo "=== Запуск скрипта install_kafka_packages.sh ==="
 # Загружаем и запускаем скрипт установки Kafka пакетов
 curl -s https://raw.githubusercontent.com/Kh-1976/scripts/main/install_kafka_packages.sh | bash
