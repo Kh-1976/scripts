fd6a906dac33e694c9cc65f64380d03ad8cacc08
Kh-1976 <78970676+Kh-1976@users.noreply.github.com>
2026-03-19 23:41:44 +0300

    Update start_minikube_helm_airflow.sh
    Удалил задержку на 7 минут. Не требуется.

--- start_minikube_helm_airflow.sh
+++ start_minikube_helm_airflow.sh
@@ -6,11 +6,6 @@ helm repo update
 echo "=== Запуск Minikube ==="
 minikube start
 
-echo "=== Ожидание 7 минут для полного запуска всех подов ==="
-echo "Ожидание началось: $(date)"
-sleep 420  # 7 минут = 420 секунд
-echo "Ожидание завершено: $(date)"
-
 echo "=== Запуск скрипта install_kafka_packages.sh ==="
 # Загружаем и запускаем скрипт установки Kafka пакетов
 curl -s https://raw.githubusercontent.com/Kh-1976/scripts/main/install_kafka_packages.sh | bash
